24e2185d04e4f7a29837b174519c7f877cee457c
troc-stack <trocades.jow@gmail.com>
2026-08-17 18:11:08 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,10 +1,15 @@
 import requests
 
+acesses = 'tG083bF4SVd6YYXm9yMxL4ZkTHkEGopS'
+
+url = f"https://api.mercadolibre.com/items/MLB2078472307"
+
 headers = {
+    "Authorization": f"Bearer {acesses}",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
 
-a = requests.get('https://www.mercadolivre.com.br/unidade-de-estado-solido-interna-m2-nvme-pcie-40-de-1-tb-de-alta/p/MLB2078472307', headers=headers)
+a = requests.get(url, headers=headers)
 
 
 print("Status:", a.status_code)
@@ -13,6 +18,3 @@ print("Tamanho:", len(a.text))
 print("Content-Type:", a.headers.get("Content-Type"))
 
 print(a.text[:2000])
-
-
-print(f'Falha no acesso: {a.status_code}')
