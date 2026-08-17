dd3d864bea492aab3cfb3c5d86de945b31ddb1f3
troc-stack <trocades.jow@gmail.com>
2026-08-17 20:26:17 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,16 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
-acesses = 'tG083bF4SVd6YYXm9yMxL4ZkTHkEGopS'
-
-url = f"https://api.mercadolibre.com/items/MLB2078472307"
-
-headers = {
-    "Authorization": f"Bearer {acesses}",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-}
-
-a = requests.get(url, headers=headers)
-
+a = requests.get('https://quotes.toscrape.com')
+soup = BeautifulSoup(a.text, "html.parser")
+print(soup)
 
 print("Status:", a.status_code)
 print("URL:", a.url)
