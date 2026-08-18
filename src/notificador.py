d83ae975d7ae9151d24474fc726a32581db02972
troc-stack <trocades.jow@gmail.com>
2026-08-18 09:16:38 -0300

    Update notificador.py

--- src/notificador.py
+++ src/notificador.py
@@ -1,18 +1,28 @@
 import json
 
-def notificador(): 
+def validar(x): 
     try:
         with open("config.json", "r", encoding="utf-8") as arquivo: 
             dados = json.load(arquivo)
     except FileNotFoundError:
         print('arquivo não encontrado!')
+        return []
+
+    Notificar_produtos = []
 
     for produto in dados['produtos']:
-        if produto['preço_alvo'] >= x:
-            pass # Notificar
-
+        if produto['preco_alvo'] >= x:
+            Notificar_produtos.append(produto.copy()) # Notificar
         else: 
-            pass #Não notificar 
+            pass #Não notificar
+
+    return Notificar_produtos
+
+def menssagem():
+    pass
+
+def notificar(): 
+    pass
 
 
 
