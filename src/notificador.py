8e226c059a420606b8bb614da1501eec9801d1d2
troc-stack <trocades.jow@gmail.com>
2026-08-18 08:44:26 -0300

    validar preço

--- src/notificador.py
+++ src/notificador.py
@@ -1,3 +1,20 @@
+import json
+
 def notificador(): 
-    pass 
-    # Notificar a pessoa caso os preços abaixem
+    try:
+        with open("config.json", "r", encoding="utf-8") as arquivo: 
+            dados = json.load(arquivo)
+    except FileNotFoundError:
+        print('arquivo não encontrado!')
+
+    for produto in dados['produtos']:
+        if produto['preço_alvo'] >= x:
+            pass # Notificar
+
+        else: 
+            pass #Não notificar 
+
+
+
+
+
